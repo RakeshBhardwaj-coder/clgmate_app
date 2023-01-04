c1715968add76f9b53773d0d05127d8b259053c3
Rakesh Bhardwaj <57101915+RakeshBhardwaj-coder@users.noreply.github.com>
2023-01-05 00:36:09 +0530

    Initial commit

---
+++ app/src/main/java/uchiha/app/clgmate_app/Register.java
@@ -0,0 +1,38 @@
+package uchiha.app.clgmate_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Register extends AppCompatActivity {
+
+    Button btn1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        btn1 = findViewById(R.id.button_register);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+
+            }
+        });
+
+        TextView btn = findViewById(R.id.already_have_account);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+
+    }
+}
